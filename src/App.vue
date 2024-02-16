bed714d8dab670c1450274efd1b2301a90d9cdf4
HovhannesK <hovhanneskeshishyan@gmail.com>
2024-02-17 01:18:35 +0400

    add app loader

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <MainPage/>
+  <BaseLoader v-if="is_loading"/>
+  <div v-show="!is_loading" class="app-container">
+    <TheHeader/>
+    <MainPage @loading-change="handleLoadingChange"/>
+    <TheFooter/>
+  </div>
 </template>
 
 <script>
+import BaseLoader from "./utils/BaseLoader.vue";
 import MainPage from "./pages/MainPage.vue";
+import TheHeader from "./components/TheHeeader.vue";
+import TheFooter from "./components/TheFooter.vue";
 
 export default {
   name: "App",
-  components: {MainPage}
+  components: {TheFooter, TheHeader, BaseLoader, MainPage},
+
+  data() {
+    return {
+      is_loading: false
+    }
+  },
+
+  methods: {
+    handleLoadingChange(is_loading) {
+      this.is_loading = is_loading;
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
+.app-container {
+  margin: 5rem;
+  box-shadow: 0 0 2px 2px #03A9F4;
+  border-radius: 1rem;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  max-width: 700px;
+  width: 100%;
+}
 </style>
---
+++ src/utils/BaseLoader.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="loader-container">
+    <div class="water"></div>
+    <p>Loading...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseLoader"
+}
+</script>
+
+<style lang="scss" scoped>
+.loader-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999;
+  flex-direction: column;
+  height: 100%;
+  min-height: 100vh;
+  min-width: 100vw;
+  background-image: linear-gradient(315deg, #485461, #28313b 74%);
+
+  p {
+    margin-top: 10px;
+    color: #fff;
+  }
+
+  .water {
+    display: flex;
+    width: 100px;
+    height: 100px;
+    background-color: skyblue;
+    border-radius: 50%;
+    margin: 0 auto;
+    position: relative;
+    box-shadow: inset 0 0 30px 0 rgba(0, 0, 0, 0.5),
+    0 4px 10px 0 rgba(0, 0, 0, 0.5);
+    overflow: hidden;
+
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      width: 100px;
+      height: 100px;
+      top: -50px;
+      background-color: #fff;
+    }
+
+    &::before {
+      border-radius: 45%;
+      background: rgba(255, 255, 255, 0.7);
+      animation: wave 5s linear infinite;
+    }
+
+    &::after {
+      border-radius: 35%;
+      /* background:rgba(28, 131, 54, 0.3); */
+      animation: wave 5s linear infinite;
+    }
+  }
+}
+
+@keyframes wave {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
